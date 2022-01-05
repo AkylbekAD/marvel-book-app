4ceb7db359bb3852f0f434bc418a7585999b9876
Akylbek <Abdykerimov.Akylbek@yandex.ru>
2022-01-05 20:12:14 +0700

    get better

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
-import  {MainPage, ComicsPage} from '../pages';
+import MainPage  from '../pages/MainPage';
+import ComicsPage from '../pages/ComicsPage'
 import AppHeader from "../appHeader/AppHeader";
 
 
